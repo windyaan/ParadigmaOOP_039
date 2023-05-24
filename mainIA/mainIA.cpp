2fce15195df94ab871f5e3962253ea0e59c433e0
windyaan <windyanurmaretha@gmail.com>
2023-05-24 20:12:38 +0700

    Fungsi utama untuk menjalankan program (menambahkan, menampilkan, menghapus)

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -7,17 +7,24 @@ using namespace std;
 
 int main() {
     ibu* varIbu = new ibu("dini");
-    anak* varAnak = new anak("tono");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("Dewi");
 
-    varIbu->tambahAnak(varAnak);
+    varIbu->tambahAnak(varAnak1);
     varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
 
     varIbu->cetakAnak();
+    varIbu2->cetakAnak();
 
     delete varIbu;
-    delete varAnak;
+    delete varIbu2;
+    delete varAnak1;
     delete varAnak2;
+    delete varAnak3;
 
     return 0;
 }
